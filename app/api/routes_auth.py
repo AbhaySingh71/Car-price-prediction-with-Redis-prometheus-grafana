30d96dccea2d1d5e18aa61197211db3297126c02
Abhay Singh <abhaysingh71711@gmail.com>
2025-09-20 15:42:22 +0530

    add route auth

---
+++ app/api/routes_auth.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from app.core.security import create_token 
+
+router = APIRouter()
+
+class AuthInput(BaseModel):
+    username: str
+    password: str
+    
+@router.post("/login")
+def login(auth: AuthInput):
+    # In a real application, validate username and password from a database
+    if auth.usename == "admin" and auth.password == "admin":
+        token = create_token({"sub": auth.username})
+        return {'access_token': token}
+    return {'error': 'Invalid credentials'}
